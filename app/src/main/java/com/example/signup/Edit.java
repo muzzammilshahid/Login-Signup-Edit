4f313111ac408eb19abb2dd5e81e8f0b15e58f31
Muzzammil <muzzammil.shahid.311@gmail.com>
2020-10-20 14:29:28 +0500

    Warnings remove

--- app/src/main/java/com/example/signup/Edit.java
+++ app/src/main/java/com/example/signup/Edit.java
@@ -2,7 +2,6 @@ package com.example.signup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import pk.codebase.requests.HttpError;
 import pk.codebase.requests.HttpRequest;
 import pk.codebase.requests.HttpResponse;
 
@@ -34,60 +32,51 @@ public class Edit extends AppCompatActivity {
         tv3.setText(getIntent().getStringExtra("number"));
         tv4.setText(getIntent().getStringExtra("username"));
         btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btn.setOnClickListener(v -> {
 
-                String first = tv.getEditableText().toString();
-                String last = tv1.getEditableText().toString();
-                //String pass = password.getEditText().getText().toString();
-                //String cpass = confirmpassword.getEditText().getText().toString();
-                String emaill = tv2.getEditableText().toString();
-                String uname = tv4.getEditableText().toString();
-                String number = tv3.getEditableText().toString();
-                HttpRequest request = new HttpRequest();
-                request.setOnResponseListener(new HttpRequest.OnResponseListener() {
-                    @Override
-                    public void onResponse(HttpResponse response) {
-                        if (response.code == HttpResponse.HTTP_OK) {
-                            System.out.println(response.toJSONObject());
-                            String first = tv.getText().toString();
-                            String last = tv1.getText().toString();
-                            String uname = tv4.getText().toString();
-                            String mobile = tv3.getText().toString();
-                            String email = tv2.getText().toString();
-                            Intent intent = new Intent(Edit.this,AfterLogin.class);
-                            intent.putExtra("firstname",first);
-                            intent.putExtra("lastname",last);
-                            intent.putExtra("username",uname);
-                            intent.putExtra("mobile",mobile);
-                            intent.putExtra("email",email);
-                            startActivity(intent);
+            String first = tv.getEditableText().toString();
+            String last = tv1.getEditableText().toString();
+            //String pass = password.getEditText().getText().toString();
+            //String cpass = confirmpassword.getEditText().getText().toString();
+            String emaill = tv2.getEditableText().toString();
+            String uname = tv4.getEditableText().toString();
+            String number = tv3.getEditableText().toString();
+            HttpRequest request = new HttpRequest();
+            request.setOnResponseListener(response -> {
+                if (response.code == HttpResponse.HTTP_OK) {
+                    System.out.println(response.toJSONObject());
+                    String first1 = tv.getText().toString();
+                    String last1 = tv1.getText().toString();
+                    String uname1 = tv4.getText().toString();
+                    String mobile = tv3.getText().toString();
+                    String email = tv2.getText().toString();
+                    Intent intent = new Intent(Edit.this,AfterLogin.class);
+                    intent.putExtra("firstname", first1);
+                    intent.putExtra("lastname", last1);
+                    intent.putExtra("username", uname1);
+                    intent.putExtra("mobile",mobile);
+                    intent.putExtra("email",email);
+                    startActivity(intent);
 
-                        }
-                    }
-                });
-                request.setOnErrorListener(new HttpRequest.OnErrorListener() {
-                    @Override
-                    public void onError(HttpError error) {
-                        // There was an error, deal with it
-                    }
-                });
-
-                JSONObject json;
-                try {
-                    json = new JSONObject();
-                    json.put("firstname", first);
-                    json.put("lastname", last);
-                    //json.put("password",pass);
-                    json.put("email",emaill);
-                    json.put("username",uname);
-                    json.put("mobile",number);
-                } catch (JSONException ignore) {
-                    return;
                 }
-                request.put("http://codebase.pk:7000/api/users/", json);
+            });
+            request.setOnErrorListener(error -> {
+                // There was an error, deal with it
+            });
+
+            JSONObject json;
+            try {
+                json = new JSONObject();
+                json.put("firstname", first);
+                json.put("lastname", last);
+                //json.put("password",pass);
+                json.put("email",emaill);
+                json.put("username",uname);
+                json.put("mobile",number);
+            } catch (JSONException ignore) {
+                return;
             }
+            request.put("http://codebase.pk:7000/api/users/", json);
         });
     }
 }
